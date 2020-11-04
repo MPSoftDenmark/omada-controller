acff3e99059982a0c51ec9b44a6a690e778ed161
Martin Dahl-Larsen <>
2020-11-04 15:09:36 +0100

    testing

---
+++ bootUp.sh
@@ -0,0 +1,27 @@
+
+mkdir -p volumenes/{logs,data,work}
+
+sudo chown -R 1000 volumenes
+chmod 700 volumenes
+
+
+
+docker run -d --rm \
+ --name omada-controller \
+ -e TZ=Europe/Copenhagen \
+ -v $(pwd)/volumenes/logs:/omada/logs \
+ -v $(pwd)/volumenes/data:/omada/data \
+ -v $(pwd)/volumenes/data:/omada/work \
+ pcarorevuelta/omada-controller
+
+
+
+
+docker run --rm \
+ --name omada-controller \
+ -e TZ=Europe/Copenhagen \
+ -v $(pwd)/volumenes/logs:/omada/logs \
+ -v $(pwd)/volumenes/data:/omada/data \
+ -v $(pwd)/volumenes/data:/omada/work \
+ pcarorevuelta/omada-controller
+
